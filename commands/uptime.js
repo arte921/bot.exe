fe1950fcc97aa4cfcf193e99b3ceedc9dd5d499b
Ard-Jan van Etten <ardjanve@gmail.com>
2020-07-25 14:02:13 +0200

    werks

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,9 +1,7 @@
-module.exports = async (msg, argstring) => { 
-    let diff = new Date() - startdate
-    let seconds = Math.floor(diff / 1000)
+module.exports = async msg => {
+    let seconds = Math.floor(process.uptime())
     let mins = Math.floor(seconds / 60)
     let hours = Math.floor(mins / 60)
     let days = Math.floor(hours / 24)
     msg.channel.send(`${days} days, ${hours % 24} hours, ${mins % 60} minutes, ${seconds % 60} seconds`)
-    
 }
