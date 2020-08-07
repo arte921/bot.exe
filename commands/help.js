ef1e1cda904c005d863139c2e5ae341d18de1994
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-07 16:10:45 +0200

    update help to ignore stuff on blocklist

--- commands/help.js
+++ commands/help.js
@@ -3,7 +3,16 @@ const path = process.cwd()
 const copypasta = JSON.parse(fs.readFileSync(path + "/storage.json").toString())
 const config = JSON.parse(fs.readFileSync(path + "/config.json").toString())
 
-let commands = fs.readdirSync(path + "/commands").join("\n").replace(/.js/g, "")
+let commands = fs
+    .readdirSync(path + "/commands")
+    .filter(
+        (command) => !config.blocklist.some(
+            blockedcommand => command.includes(blockedcommand)
+        )
+    )
+    .join("\n")
+    .replace(/.js/g, "")
+
 let starttext = `
 All commands prefixed with ${config.prefix}, without additional spaces.
 
