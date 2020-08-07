55952616851bfe3feea3206567e621a0976d6dd4
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-07 16:37:54 +0200

    cleanup update

--- commands/update.js
+++ commands/update.js
@@ -7,7 +7,7 @@ const config = JSON.parse(fs.readFileSync(path + "/config.json").toString())
 
 module.exports = async (msg, argstring) => {
     if (!config.sshusers.includes(msg.author.id)) return
-    const child = exec("git pull", (error, stdout, stderr) => {
+    exec("git pull", (error, stdout, stderr) => {
         msg.channel.send(error + stderr + stdout).catch(e => console.log(e))
     })
 }
