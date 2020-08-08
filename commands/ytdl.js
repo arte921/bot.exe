8fbf6d9acf3aebd00cae6207f73ec6f85469eca6
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-08 20:29:41 +0200

    update ytdl

--- commands/ytdl.js
+++ commands/ytdl.js
@@ -1,8 +1,14 @@
 const fs = require("fs");
 const ytdl = require("ytdl-core");
+const path = process.cwd();
 
 module.exports = (msg, argstring, config) => {
-    const file = "./temp/" + argstring.substr(-11);
-    ytdl(argstring).pipe(fs.createWriteStream(file));
-    msg.channel.send({files: [file]});
+
+    const file = path + "/temp/" + argstring.substr(-11) + ".mp3";
+    ytdl(argstring, { filter: "audioonly" }).pipe(fs.createWriteStream(file)).on("finish", async () => {
+        await msg.channel.send({files: [file]}).catch((e) => {
+            msg.channel.send("file too large!");
+        });
+        fs.unlinkSync(file);
+    });
 };
