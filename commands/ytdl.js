50ec101b025fe0380b4c9c8801a0efe8d6508f22
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-08 19:49:49 +0200

    add temp dir, ytdl

--- commands/ytdl.js
+++ commands/ytdl.js
@@ -2,8 +2,7 @@ const fs = require("fs");
 const ytdl = require("ytdl-core");
 
 module.exports = (msg, argstring, config) => {
-    const file = "../temp/" + argstring.substr(-11);
+    const file = "./temp/" + argstring.substr(-11);
     ytdl(argstring).pipe(fs.createWriteStream(file));
-    const attachment = new MessageAttachment(file);
-    msg.channel.send(attachment);
+    msg.channel.send({files: [file]});
 };
