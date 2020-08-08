3d056fa7582ec989a8b5d4a3a06693b8080eb758
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-08 19:01:28 +0200

    music,js now per channel

--- commands/music.js
+++ commands/music.js
@@ -3,25 +3,31 @@ const fs = require("fs");
 
 const globalconfig = JSON.parse(fs.readFileSync("./config.json").toString());
 
-let dispatcher, lastseenchannel;
+let channels = {}
 
 module.exports = async (msg, argstring, config) => {
     if (!globalconfig.caching) {    // to prevent users from being able to start music, but not control it
         msg.channel.send("This command only works if command caching is enabled.")
         return
     }
+
+    if (!msg.member.voice.channel) {
+        msg.channel.send("You need to join a voice channel to use the music command :)");
+        return;
+    }
+
+    const channel = !msg.member.voice.channel.id;
+    console.log(channel);
     
     let splitargstring = argstring.split(" ");
     switch (splitargstring[0]) {
         case "play":
-            if (!lastseenchannel && !msg.member.voice.channel) {
-                msg.channel.send("You need to join a voice channel to start the music command :)");
-                return;
-            }
-            if (msg.member.voice.channel)
-                lastseenchannel = msg.member.voice.channel;
-            let connection = await lastseenchannel.join();
-            dispatcher = connection.play(
+
+            channels[channel] = {}
+
+            if (msg.member.voice.channel) channels[channel].dcchannel = msg.member.voice.channel;
+            let connection = await channels[channel].dcchannel.join();
+            channels[channel].dispatcher = connection.play(
                 ytdl(
                     argstring.indexOf("youtube") < 0
                         ? "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
@@ -32,28 +38,28 @@ module.exports = async (msg, argstring, config) => {
             break;
         case "pause":
             try {
-                dispatcher.pause();
+                channels[channel].dispatcher.pause();
             } catch (e) {
                 msg.channel.send("Nothing playing!");
             }
             break;
         case "resume":
             try {
-                dispatcher.resume();
+                channels[channel].dispatcher.resume();
             } catch (e) {
                 msg.channel.send("Nothing playing!");
             }
             break;
         case "stop":
             try {
-                dispatcher.destroy();
+                channels[channel].dispatcher.destroy();
             } finally {
-                lastseenchannel.leave();
+                channels[channel].dcchannel.leave();
             }
             break;
         case "volume":
             try {
-                dispatcher.setVolume(splitargstring[1] / 100);
+                channels[channel].dispatcher.setVolume(splitargstring[1] / 100);
             } catch (e) {
                 msg.channel.send("Nothing playing!");
             }
