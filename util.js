13b768a837596db08a35ba54cc71077ad9d1fb5a
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-08 12:27:41 +0200

    reformat util.js

--- util.js
+++ util.js
@@ -1,17 +1,20 @@
 module.exports = {
-    getCustomEmote: (cache, name) => cache.find(emoji => emoji.name == name),
+    getCustomEmote: (cache, name) => cache.find((emoji) => emoji.name == name),
     sendLongMessage: (channel, message, markup = "") => {
-        let chunksize = 1500
-        let lastindex = 0
-        let i = 0
+        let chunksize = 1500;
+        let lastindex = 0;
+        let i = 0;
         while (i <= message.length) {
-            i+= chunksize
-            channel.send(markup + message.slice(lastindex, i) + markup)
-            lastindex = i
-
+            i += chunksize;
+            channel.send(markup + message.slice(lastindex, i) + markup);
+            lastindex = i;
         }
     },
-    mock: text => text.split("").map((char, index) => {
-            return (index % 2 == 0) ? char.toLowerCase() : char.toUpperCase()
-        }).join("")    
-}
+    mock: (text) =>
+        text
+            .split("")
+            .map((char, index) => {
+                return index % 2 == 0 ? char.toLowerCase() : char.toUpperCase();
+            })
+            .join(""),
+};
