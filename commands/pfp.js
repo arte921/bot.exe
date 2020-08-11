e4c8e2eb29be7f4bee865dc91ee58e151fc6e6dd
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-11 15:06:44 +0200

    higher res

--- commands/pfp.js
+++ commands/pfp.js
@@ -1,5 +1,5 @@
 module.exports = async (msg, argstring, config) => {
     const user = msg.mentions.users.first() || msg.author;
-    const url = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png`;
+    const url = `https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=4096`;
     msg.channel.send(url);
 }
