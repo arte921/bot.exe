1e09d7eff877a3d66dbcba0d6e356a35fb5c7d3b
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-12 11:33:59 +0200

    updated help

--- assets/generate_help.js
+++ assets/generate_help.js
@@ -1,31 +1,31 @@
 const helpobject = {
 ban:`
-Usage: ban [mention of user to ban].
+Usage: \`ban [mention of user to ban]\`.
 
 Requires banning privilege for both the bot and whoever issues the command.
 `, cat:`
 This command will return a random cat image.
 `, curse:`
-Usage: curse [text to curse].
+Usage: \`curse [text to curse]\`.
 
 This command will return all text behind the command, but stylized ̷͗͘L̷̓͐I̸͗͝K̵̓̓E̴̾̔ ̴̆͠T̶̛̾H̷̋̈́I̸͗͝Ś̷͐.
 `, dmme:`
-Usage: dmme (text to send)
+Usage: \`dmme (text to send\`)
 
 Will send a dm (private message) to the user who issued the command.
 The message will contain the given text, or alternatively a placeholder text.
 `, doom:`
 This command is experimental. It will return an animation of 2 spheres using a custom ray marcher.
 `, emoji:`
-Usage: emoji [maximum amount of emoji per word] [text to put emojis in].
+Usage: \`emoji [maximum amount of emoji per word] [text to put emojis in]\`.
 
 Note that too long messages with too much emojis will not be returned, as per discord character limit.
 `, emote:`
-Usage: emote [discord server custom emote].
+Usage: \`emote [discord server custom emote]\`.
 
 This will not work with unicode or default discord emotes.
 `, fancy:`
-Usage: fancy [text to stylize].
+Usage: \`fancy [text to stylize]\`.
 
 Returns the text but stylized 𝕃𝕀𝕂𝔼 𝕋ℍ𝕀𝕊.
 `, flip:`
@@ -33,19 +33,19 @@ Returns the text but stylized 𝕃𝕀𝕂𝔼 𝕋ℍ𝕀𝕊.
 
 Has a 50% chance of returning \`true\`, returns \`false\` otherwise.
 `, help:`
-Usage: help (command).
+Usage: \`help (command)\`.
 
 Will return a list of available command if no arguments given, will return the help page of a specific command if an argument is given, for an existing command which is enabled in the server it's requested from.
 `, kick:`
-Usage: kick [metion of user to kick].
+Usage: \`kick [metion of user to kick]\`.
 
 Requires kicking privilege for both the bot and whoever issues the command.
 `, mock:`
-Usage: mock [text to mock].
+Usage: \`mock [text to mock]\`.
 
 Will return the given text stylized lIkE ThIs
 `, music:`
-Usage: music [command].
+Usage: \`music [command]\`.
 
 Available music commands:
     play [youtube url or video id]
@@ -65,7 +65,7 @@ Available music commands:
     
 Requires command caching to be enabled by the bot sysadmin due to technical reasons.
 `, pfp:`
-Usage: pfp (mention)
+Usage: \`pfp (mention)\`
 
 Will return a full res version of the mentioned user's profile picture, or the message author if none is given.
 `, ping:`
@@ -75,19 +75,19 @@ Will return a text plus the name of who sent the text.
 
 This guarantees a fully random result, as per laws of physics. Result might be a bit slower than the normal flip command, but is more ~~overkill~~ random. 
 `, remindme:`
-Usage: remindme [time] (note).
+Usage: \`remindme [time] (note)\`.
 
 Will ping you with an optional note you give after the specified amount of *minutes*.
 `, say:`
-Usage: say [text].
+Usage: \`say [text]\`.
 
 Will simply repeat you by sending the given text.
 `, scream:`
-Usage: scream [text].
+Usage: \`scream [text]\`.
 
 Will return the given text but stylized ***L I K E   T H I S***.
 `, search:`
-Usage: search [search query].
+Usage: \`search [search query]\`.
 
 Will help you search for the query.
 `, update:`
@@ -95,6 +95,19 @@ Sysadmin only.
 
 Will update the bot from github to the latest version. Clearing the command cache using the clearcache command might be neccesary.
 `, uptime:`
+Returns the uptime of the bot.
+`, uwu:`
+Usage: \`uwu [text]\`.
+
+Will return the given text but stylized wike this.
+`, whisper:`
+Usage: \`whisper [text]\`.
+
+Will return the given text but stylized ˡⁱᵏᵉ ᵗʰⁱˢ.
+`, ytdl:`
+Usage: \`ytdl [youtube url/video id]\`.
+
+Will return an mp3 file with the audio of the given youtube video. Will not work on longer tracks thanks do the discord 8mb file size limit.
 `
 };
 
--- commands/help.js
+++ commands/help.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const details = JSON.parse(fs.readFileSync("./assets/help.json"));
 
 module.exports = async (msg, argstring, config) => {
     if (argstring == "") {
@@ -22,11 +23,11 @@ Available commands:
     
         msg.channel.send(starttext + commands);
     } else {
-        const details = JSON.parse(fs.readFileSync("./assets/help.json"));
         if (!config.blocklist.includes(argstring) && details[argstring]) {
             msg.channel.send(details[argstring]);
         } else {
             msg.channel.send(`No help page found for "${argstring}"`);
+            console.log(details[argstring]);
         }
     }
 
