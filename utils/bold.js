e635e6f2a331636d7b24a0498c7f49360999c883
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-13 19:26:01 +0200

    bold naem

--- utils/bold.js
+++ utils/bold.js
@@ -1,6 +1,11 @@
+let longest = 0; // no work yet because no good caching
+
 module.exports = (msg, config) => {
-    if (config.errands.configs.boldchannels.includes(msg.channel.id)) {    
+    if (config.errands.configs.boldchannels.includes(msg.channel.id)) {
         msg.delete().catch((e) => {});
-        msg.channel.send("**" + msg.content + "**");
+        if (msg.author.username.length > longest) longest = msg.author.username.length;
+
+        const spaces = longest - msg.author.username.length;
+        msg.channel.send("`" + msg.author.username + "`: " + " ".repeat(spaces) + "**" + msg.content + "**");
     }
 }
