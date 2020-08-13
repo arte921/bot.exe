c6e095de28a03b9408cf06e9082777e468a3e855
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-13 17:16:42 +0200

    fix bold

--- utils/bold.js
+++ utils/bold.js
@@ -1,5 +1,5 @@
 module.exports = (msg, config) => {
-    if (config.boldchannels.includes(msg.channel.id)) {    
+    if (config.errands.configs.boldchannels.includes(msg.channel.id)) {    
         msg.delete().catch((e) => {});
         msg.channel.send("**" + msg.content + "**");
     }
