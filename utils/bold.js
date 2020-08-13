4bfc0ed41ab382da4337330ec16d007497112e20
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-13 19:31:27 +0200

    cleanup bold

--- utils/bold.js
+++ utils/bold.js
@@ -1,11 +1,6 @@
-let longest = 0; // no work yet because no good caching
-
 module.exports = (msg, config) => {
     if (config.errands.configs.boldchannels.includes(msg.channel.id)) {
         msg.delete().catch((e) => {});
-        if (msg.author.username.length > longest) longest = msg.author.username.length;
-
-        const spaces = longest - msg.author.username.length;
-        msg.channel.send("`" + msg.author.username + "`: " + " ".repeat(spaces) + "**" + msg.content + "**");
+        msg.channel.send("`" + msg.author.username + "`: " + "**" + msg.content + "**");
     }
 }
