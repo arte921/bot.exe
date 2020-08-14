beb4f7c390c1aabdb4eca6403d68c98ab39416de
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-14 17:08:04 +0200

    hug

--- commands/hug.js
+++ commands/hug.js
@@ -4,18 +4,20 @@ const midhugs = ["⊂（♡⌂♡）⊃", "⊂(◉‿◉)つ", "＼(^o^)／", "d
 
 const getrandom = (array) => array[Math.floor(Math.random() * array.length)];
 
+const approveemoji = "✅";
+
 module.exports = async (msg, argstring, config) => {
     const reciever = msg.mentions.users.first();
     if (reciever) {
         let starthug = getrandom(lefthugs);
         msg.channel.send(`**${msg.author.username}: ${starthug} ${argstring} ?**`).then(botmsg => {
-            botmsg.react("✅");
+            botmsg.react(approveemoji);
             botmsg.awaitReactions(
-                (reaction, reacter) => reacter.id == reciever.id,//reaction.emoji.name == "✅" && 
+                (reaction, reacter) => reaction.emoji.name == approveemoji && reacter.id == reciever.id,//
                 { max: 1, time: 60000, errors: ['time'] }
-            ).then(collected => {
+            ).then(() => {
                 botmsg.edit(`**${msg.author.username} ${starthug} ${getrandom(righthugs)} ${reciever.username}**`);
-            }).catch((e) => {});
+            }).catch(() => {});
         });
     } else {
         msg.channel.send(getrandom(midhugs));
