4aacbfa18b58a4d3c4329e508ecc2ea19eeab152
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-14 21:49:10 +0200

    add profanity filter

--- utils/profanity.js
+++ utils/profanity.js
@@ -1,9 +1,20 @@
-const badwords = ["simp", "lmao"];
+const badwords = ["simp"];
 
 module.exports = (msg, config) => {
-    if (config.errands.configs.profanity) {
-        badwords.find
-        const words = msg.content.split(" ");
+    let naughty = false;
+    let words = msg.content.split(" ");
+    
+    words = words.map(word => {
+        if (badwords.includes(word)) {
+            naughty = true;
+            return "#".repeat(word.length);
+        } else {
+            return word;
+        }
+    });
+    
+    if (naughty) {
         msg.delete().catch((e) => {});
+        msg.channel.send("`" + msg.author.username + "`: " + words.join(" "));
     }
 }
