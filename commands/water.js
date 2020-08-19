f979ac01d97ec5cf4f610ae7728af0d92141b954
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-19 20:13:48 +0200

    worter

--- commands/water.js
+++ commands/water.js
@@ -3,6 +3,6 @@ const cwd = process.cwd();
 const file = path.resolve(cwd, "assets", "water.png");
 
 module.exports = async (msg, argstring, config) => {
-    msg.channel.send("**I HAVE ARE HAS OBTAINED THE CRAVED *WATER***");
+    msg.channel.send("**I HAVE ARE HAS OBTAINED THE CRAVED *W A T E R***");
     msg.channel.send({files: [file]});
 };
