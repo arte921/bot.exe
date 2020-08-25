bcdc98adff6449729954dbadb08f49b0dba5d5e8
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-25 19:38:28 +0200

    servericon

---
+++ commands/servericon.js
@@ -0,0 +1,13 @@
+const path = require("path");
+const cwd = process.cwd();
+const { save, load, file } = require(path.join(cwd, "database", "index.js"));
+const permissions = file([cwd, "utils", "permissions.json"]);
+const errors = file([cwd, "utils", "errors.json"]);
+
+module.exports = {
+    permission: permissions.member,
+    code: async (msg, argstring, config) => msg.guild.iconURL(),
+    help: `
+    Returns a text plus the name of who sent the text.
+    `
+}
