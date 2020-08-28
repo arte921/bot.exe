fd777425feac8cb3119aa04a39b03ebf0638d1bc
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-28 11:09:11 +0200

    rename dump -> train, improve chat

--- commands/chat.js
+++ commands/chat.js
@@ -4,6 +4,19 @@ const cwd = process.cwd();
 const { save, load, file } = require(path.join(cwd, "database", "index.js"));
 const { permissions, errors } = require(path.join(cwd, "utils", "constants.js"));
 
+const getresponse = (messages, input, test) => {
+    let responses = [];
+    messages.forEach((message, index) => {
+        if (test(message, input)) {
+            responses.push(messages[index - 1]);
+        }
+    });
+
+    return responses
+}
+
+const getrandom = (array) => array[Math.floor(Math.random() * array.length)];
+
 // TODO prevent errors on incorrect input
 module.exports = {
     permission: permissions.member,
@@ -18,19 +31,23 @@ module.exports = {
                 .replace(/`/g, "")
             );
         
-        let responses = [];
-        
-        messages.forEach((message, index) => {
-            if (message == input) {
-                responses.push(messages[index - 1]);
+        const tests = [
+            (message, input) => message == input,
+            (message, input) => message.includes(input),
+            (message, input) => input.includes(message)
+        ];
+
+        let responses;
+        for (const test of tests) {
+            const result = getresponse(messages, input, test);
+            if (result.length > 0) {
+                responses = result;
+                break;
             }
-        });
-        
-        if (responses.length > 0) {
-            return responses[Math.floor(Math.random() * responses.length)];
-        } else {
-            return "I don't understand :(";
         }
+
+        if (responses) return getrandom(responses);
+        else return "I don't understand :(";
     },
     help: `
     Usage: \`chat message\`.
