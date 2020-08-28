53e3a0a56beb0d68b89877366c9fd2dfe44b3dfe
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-28 11:31:17 +0200

    train stats

--- commands/train.js
+++ commands/train.js
@@ -4,9 +4,12 @@ const cwd = process.cwd();
 const { save, load, file } = require(path.join(cwd, "database", "index.js"));
 const { permissions, errors } = require(path.join(cwd, "utils", "constants.js"));
 
+const messagelimit = 100;
+
 module.exports = {
-    permission: permissions.sysadmin,
+    permission: permissions.member,
     code: async (msg, argstring, config) => {
+        const starttime = Date.now();
         msg.react("🛠");
         let lastid = msg.id;
 
@@ -14,7 +17,7 @@ module.exports = {
 
         while (true) {
             const messages = (await msg.channel.messages.fetch({
-                limit: 100,
+                limit: messagelimit,
                 before: lastid
             })).map(message => {
                 return {
@@ -24,18 +27,17 @@ module.exports = {
             });
 
             lastid = messages[messages.length - 1].id;
-            console.log(lastid, messages.length);
 
             allmessages.push(...messages.map(message => message.content));
             
-            if (messages.length < 100) break;
+            if (messages.length < messagelimit) break;
         }
 
         console.log(allmessages.length);
 
         const file = path.join("temp", `${msg.channel.id}_chat.json`);
         await writeFile(file, JSON.stringify(allmessages));
-        msg.react("👍");
+        return `Indexed ${allmessages.length} messages in ${Math.round((Date.now() - starttime) / 1000)} seconds for usage in chat command.`
         // let failed = false;
         // await msg.channel.send({files: [file]}).catch(() => failed = true);
         // if (failed) return "File too large for Discord!";
