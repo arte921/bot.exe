2b67bf61e11974fad087d3bdda1ef5a04c4a05a6
Ard-Jan van Etten <ardjanve@gmail.com>
2020-08-31 17:36:07 +0200

    calculate

---
+++ commands/calculate.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const cwd = process.cwd();
+const { save, load, file } = require(path.join(cwd, "database", "index.js"));
+const { permissions, errors } = require(path.join(cwd, "utils", "constants.js"));
+const math = require("mathjs");
+
+
+module.exports = {
+    permission: permissions.sysadmin,
+    code: async (msg, argstring, config) => {
+        try{
+            return math.evaluate(argstring).toString();
+        } catch (e) {
+            return "no";
+        }
+    },
+    help: ``
+}
