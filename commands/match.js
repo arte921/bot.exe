3c8bce7e530527f8e26863725863e337751e70c9
Ard-Jan van Etten <ardjanve@gmail.com>
2020-09-02 14:29:18 +0200

    fix match

--- commands/match.js
+++ commands/match.js
@@ -8,8 +8,6 @@ module.exports = {
     code: async (msg, argstring, config) => {
         const recievers = msg.mentions.users.map(user => user.id);
 
-        if (recievers.includes("488724416579108865") && recievers.includes("732681183669780611")) return "**STOP SHIPPING US** 😭";
-
         if (recievers.length != 2) return "Please mention 2 people!";
         let total = 0;
         for (const id of recievers) total += id - 0;
