46fb1f9e4d2f43ca70f90a8b716b93f66b4ce299
Ard-Jan van Etten <arte@linux.fritz.box>
2020-10-02 15:05:24 +0200

    semi fix installer

--- database/installer.js
+++ database/installer.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const { save, load, file } = require(path.join(__dirname, "index.js"));
 
-const servers = file(__dirname, "defaults", "servers.json");
-const globalconfig = file(__dirname, "defaults", "config.json");
+const servers = file([__dirname, "defaults", "servers.json"]);
+const globalconfig = file([__dirname, "defaults", "config.json"]);
 globalconfig.default_config = file([__dirname, "default_config.json"]);
 save("config", globalconfig);
